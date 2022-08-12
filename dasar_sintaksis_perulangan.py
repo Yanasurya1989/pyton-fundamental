4aa59a5b57f49e7e96b87c6906946f05be1bad72
Yanasurya1989 <yanasurya1989@gmail.com>
2022-08-12 21:38:07 +0700

    looping

---
+++ dasar_sintaksis_perulangan.py
@@ -0,0 +1,11 @@
+"""
+Looping for read books
+"""
+print("Ibu berkata, baca semua bukumu")
+jumlah_buku = 100
+
+buku_sudah_dibaca = 0
+for buku_sudah_dibaca in range(1, jumlah_buku + 1):
+    print(f"buku ke {buku_sudah_dibaca} sudah dibaca")
+
+print(f"jumlah buku yang sudah dibaca {buku_sudah_dibaca}")
