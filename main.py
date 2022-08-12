a606d79b9bfcffb51fff3e2fb218dc7ae80ae07f
Yanasurya1989 <yanasurya1989@gmail.com>
2022-08-12 20:27:11 +0700

    add condition and print with f to count

--- main.py
+++ main.py
@@ -13,12 +13,22 @@ print("Maka budi berangkat ke toko")
 print("Dan mulai berbelanja")
 
 # percabangan
-print('-' *20)
+print('-' * 20)
 print("Percabangan")
 print('-' * 20)
+
 susu_tersedia = 100
-if susu_tersedia > 0 :
+print(f"jumlah susu tersedia {susu_tersedia} botol")
+telur_tersedia = 1000
+print(f"jumlah telur tersedia {telur_tersedia} butir")
+if susu_tersedia > 0:
     print("Budi cek uang yang dibawa sudah cukup")
-    print("Budi membeli satu boto")
+    print("Budi membeli satu botol")
+    if telur_tersedia > 0:
+        print("Budi membeli 6 butir")
 else:
     print("Budi tidak jadi membeli")
+
+
+print("Budi kembali kerumah")
+print("Dan menyampaikan hasilnya kepada ibu")
