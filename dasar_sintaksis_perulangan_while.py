73e72f94bc5c7e812f3db29f1477ed5bb355b963
Yanasurya1989 <yanasurya1989@gmail.com>
2022-08-12 22:08:20 +0700

    looping with while

---
+++ dasar_sintaksis_perulangan_while.py
@@ -0,0 +1,10 @@
+"""
+Looping for read books
+"""
+jumlah_buku = 100
+
+jumlah_buku_sudah_dibaca = 0
+
+while jumlah_buku_sudah_dibaca < jumlah_buku:
+    jumlah_buku_sudah_dibaca = jumlah_buku_sudah_dibaca + 1
+    print(f"buku ke {jumlah_buku_sudah_dibaca} sudah dibaca")
