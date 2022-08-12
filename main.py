ac0c77fe7d79fc758f393e1278895942dbf70a8b
Yanasurya1989 <yanasurya1989@gmail.com>
2022-08-12 13:54:37 +0700

    add condition

--- main.py
+++ main.py
@@ -11,3 +11,14 @@ print('Budi menjawab : "Baik apa yang harus saya lakukan ditoko?"')
 print('Ibu menjawab : "Beli 1 botol susu, dan jika ada telur beli 6"')
 print("Maka budi berangkat ke toko")
 print("Dan mulai berbelanja")
+
+# percabangan
+print('-' *20)
+print("Percabangan")
+print('-' * 20)
+susu_tersedia = 100
+if susu_tersedia > 0 :
+    print("Budi cek uang yang dibawa sudah cukup")
+    print("Budi membeli satu boto")
+else:
+    print("Budi tidak jadi membeli")
