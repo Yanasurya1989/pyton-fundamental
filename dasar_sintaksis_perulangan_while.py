17022a47d0f4b3a65e0c18054ea9fa960b12f170
Yanasurya1989 <yanasurya1989@gmail.com>
2022-08-17 13:02:12 +0700

    add confition while

--- dasar_sintaksis_perulangan_while.py
+++ dasar_sintaksis_perulangan_while.py
@@ -5,6 +5,9 @@ book_count = 10
 print('Mother said : "Read all your book"')
 
 book_read = 0
+print(f"count book already readed is {book_read}")
 while book_read < book_count:
     book_read = book_read + 1
     print(f"book {book_read} already read")
+
+print(f"now i have readed {book_read} books")
